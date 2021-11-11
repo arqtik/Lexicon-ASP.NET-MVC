73c9a740eb81ec3a9563fc8dd99d7619b8043243
arqtik <martynascamets1@gmail.com>
2021-11-11 11:32:54 +0100

    First assignment done

--- DevASPMVC/Controllers/HomeController.cs
+++ DevASPMVC/Controllers/HomeController.cs
@@ -12,5 +12,20 @@ namespace DevASPMVC.Controllers
         {
             return View();
         }
+        
+        public IActionResult About()
+        {
+            return View();
+        }
+        
+        public IActionResult Projects()
+        {
+            return View();
+        }
+        
+        public IActionResult Contact()
+        {
+            return View();
+        }
     }
 }
