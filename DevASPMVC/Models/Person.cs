f67e72b293c2ef59156cac98600e62ac23cf66a7
arqtik <martynascamets1@gmail.com>
2021-11-15 12:03:45 +0100

    Added people model

---
+++ DevASPMVC/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DevASPMVC.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Gender Gender { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+    }
+}
