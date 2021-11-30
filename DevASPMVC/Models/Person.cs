07555bdd8870bf7a0b12f6cab48a4bda7951c218
arqtik <martynascamets1@gmail.com>
2021-11-30 12:16:44 +0100

    Update Person.cs

--- DevASPMVC/Models/Person.cs
+++ DevASPMVC/Models/Person.cs
@@ -1,14 +1,30 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace DevASPMVC.Models
 {
     public class Person
     {
+        [Key]
         public int ID { get; set; }
+
+        [Required]
+        [MaxLength(20)]
         public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(20)]
         public string LastName { get; set; }
+
+        [Required]
         public Gender Gender { get; set; }
+
+        [MaxLength(255)]
         public string Address { get; set; }
+
+        [Required]
+        [Key]
+        [MaxLength(255)]
         public string Email { get; set; }
     }
 }
