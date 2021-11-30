cbee368a5042da6980e77ba8dca7638e40220a86
arqtik <martynascamets1@gmail.com>
2021-11-30 11:35:34 +0100

    Create AppDbContext.cs

---
+++ DevASPMVC/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevASPMVC.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        DbSet<Person> People { get; set; }
+    }
+}
