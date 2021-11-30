9c5ce4805e3617cf833a29b5ebd701a0a3500d7e
arqtik <martynascamets1@gmail.com>
2021-11-30 12:27:52 +0100

    Update Startup.cs

--- DevASPMVC/Startup.cs
+++ DevASPMVC/Startup.cs
@@ -1,21 +1,36 @@
+using DevASPMVC.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevASPMVC
 {
     public class Startup
     {
+        IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseMySql(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
             services.AddDistributedMemoryCache();
 
             services.AddSession(options =>
