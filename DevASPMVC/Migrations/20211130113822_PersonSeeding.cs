9e404f291bc88ce4aba463a5fc9736c09657d26f
arqtik <martynascamets1@gmail.com>
2021-11-30 12:39:39 +0100

    Added person seed

---
+++ DevASPMVC/Migrations/20211130113822_PersonSeeding.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DevASPMVC.Migrations
+{
+    public partial class PersonSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "People",
+                columns: new[] { "ID", "Address", "Email", "FirstName", "Gender", "LastName" },
+                values: new object[] { -1, "Gustav gatan 21C 54412 Gävle", "johhug@domain.com", "Johannes", 0, "Hugosson" });
+
+            migrationBuilder.InsertData(
+                table: "People",
+                columns: new[] { "ID", "Address", "Email", "FirstName", "Gender", "LastName" },
+                values: new object[] { -2, "Tidnings gatan 3F 65351 Borås", "ingand@domain.com", "Ingrid", 1, "Andersson" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "People",
+                keyColumn: "ID",
+                keyValue: -2);
+
+            migrationBuilder.DeleteData(
+                table: "People",
+                keyColumn: "ID",
+                keyValue: -1);
+        }
+    }
+}
--- DevASPMVC/Models/AppDbContext.cs
+++ DevASPMVC/Models/AppDbContext.cs
@@ -10,5 +10,29 @@ namespace DevASPMVC.Models
         }
 
         DbSet<Person> People { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>().HasData(new Person
+            {
+                ID = -1,
+                FirstName = "Johannes",
+                LastName = "Hugosson",
+                Gender = Gender.Male,
+                Address = "Gustav gatan 21C 54412 Gävle",
+                Email = "johhug@domain.com"
+            });
+
+            modelBuilder.Entity<Person>().HasData(new Person
+            {
+                ID = -2,
+                FirstName = "Ingrid",
+                LastName = "Andersson",
+                Gender = Gender.Female,
+                Address = "Tidnings gatan 3F 65351 Borås",
+                Email = "ingand@domain.com"
+            });
+
+        }
     }
 }
