37b81674811a1c625b30ee41a0e392a4e889957d
arqtik <martynascamets1@gmail.com>
2021-11-30 15:07:55 +0100

    Create City.cs

---
+++ DevASPMVC/Models/City.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DevASPMVC.Models
+{
+    public class City
+    {
+        [Key]
+        public string Name { get; set; }
+
+        [Key]
+        public Country Country { get; set; }
+
+        [Required]
+        public List<Person> People { get; set; }
+    }
+}
