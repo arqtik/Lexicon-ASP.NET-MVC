c1079f0780cc10f6b16998eb439db43d78ee6d0b
arqtik <martynascamets1@gmail.com>
2021-11-30 15:07:58 +0100

    Create Country.cs

---
+++ DevASPMVC/Models/Country.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DevASPMVC.Models
+{
+    public class Country
+    {
+        [Key]
+        public string Name { get; set; }
+
+        [Required]
+        public List<City> Cities { get; set; }
+    }
+}
