aa427d850101ff0ef8fff6dc11e0aad095c72358
arqtik <martynascamets1@gmail.com>
2021-12-06 18:27:01 +0100

    Added migrations identity

--- DevASPMVC/Migrations/20211206171219_AddedIdentity.Designer.cs
+++ DevASPMVC/Migrations/20211206171219_AddedIdentity.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using DevASPMVC.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DevASPMVC.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211206171219_AddedIdentity")]
+    partial class AddedIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
